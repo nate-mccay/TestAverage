c61a3e4bd1b679a39c977c7aa91734822fbf1332
nate mccay <nate.mccay@ymail.com>
2019-09-06 13:51:43 -0500

    testaverage

--- TestAverage/TestAverage/Program.cs
+++ TestAverage/TestAverage/Program.cs
@@ -6,7 +6,16 @@ namespace TestAverage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string TestAverage;
+            Console.WriteLine("What is your first test score?");
+            var First = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your second test score?");
+            var Second = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your third test score?");
+            var Third = int.Parse(Console.ReadLine());
+            var TestAddition = First + Second + Third;
+            var answerAverage = TestAddition / 3;
+            Console.WriteLine("Your test average is " + answerAverage);
         }
     }
 }
