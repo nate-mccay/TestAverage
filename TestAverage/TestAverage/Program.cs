c393d49de41180c437a5303a78bd2145af7910f3
nate mccay <nate.mccay@ymail.com>
2019-10-11 12:00:30 -0500

    complete

--- TestAverage/TestAverage/Program.cs
+++ TestAverage/TestAverage/Program.cs
@@ -4,8 +4,23 @@ namespace TestAverage
 {
     class Program
     {
+        static int GetUserResponse(string question)
+        {
+            int integerFromUser;
+            bool success = false;
+            do
+            {
+                Console.WriteLine(question);
+                string userResponse = Console.ReadLine();
+                success = int.TryParse(userResponse, out integerFromUser);
+            } while (success == false);
+            return integerFromUser;
+        }
         static void Main(string[] args)
         {
+            var scoreFirst = GetUserResponse("What is your first test score?");
+            var scoreSecond = GetUserResponse("What is your second test score?");
+            var scoreThird = GetUserResponse("What is your third test score?");
             string TestAverage;
             Console.WriteLine("What is your first test score?");
             var First = int.Parse(Console.ReadLine());
